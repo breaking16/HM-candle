0eb7d114922c7b6d681ec743da9628584635620f
breaking16 <w.w804.162@gmail.com>
2024-02-24 13:07:55 +0200

    20half party

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=> {
   menuMobile.classList.toggle('menu--open');
 });
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+   slidesPerView: 3,
+   spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+      640: {
+          slidesPerView: 3,
+        },
+        480: {
+          slidesPerView: 2,
+        },
+        360:{
+          slidesPerView: 1,
+        }
+     },
+});
